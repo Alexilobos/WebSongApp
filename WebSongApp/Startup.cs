4a5832ec6989d5f62ea48eca96fcb93bbe906343
Alexi Lobos <a2020117797@estudiantes.upsa.edu.bo>
2021-10-12 23:47:59 -0400

    DbContext del Startup Agregado

--- WebSongApp/Startup.cs
+++ WebSongApp/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSongApp.Data;
 
 namespace WebSongApp
 {
@@ -24,6 +26,10 @@ namespace WebSongApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // DbContext
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
