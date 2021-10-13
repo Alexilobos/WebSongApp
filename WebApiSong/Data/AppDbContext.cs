66d99246ec2520b68fe4941fb17749b4766c7b93
Alexi Lobos <a2020117797@estudiantes.upsa.edu.bo>
2021-10-13 00:25:42 -0400

    AppDbContext del Data Creado

---
+++ WebApiSong/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiSong.Models;
+
+namespace WebApiSong.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Song> Cancion { get; set; }
+    }
+}
