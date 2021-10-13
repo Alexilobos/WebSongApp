b11bc0bdb6f977e6b774f4c61bc66c801272e28f
Alexi Lobos <a2020117797@estudiantes.upsa.edu.bo>
2021-10-12 23:40:23 -0400

    Modelo y datos creados

--- WebSongApp/Models/Song.cs
+++ WebSongApp/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace WebSongApp.Models
 {
     public class Song
     {
+        [Key]
+       
+        [Required(ErrorMessage = "El campo del nombre es requerido")] //para validar el name
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "El nombre tiene que tener entre 5 a 60 caracteres")]
+        [Display(Name = "Cancion")]
+        public string Cancion { get; set; }
+        [Required]
+        [Display(Name = "Autor")]
+        public string Autor { get; set; }
+        [Display(Name = "Letra")]
+        public string Letra { get; set; }
+        [Display(Name = "Enlace")]
+        public string Enlace { get; set; }
     }
 }
