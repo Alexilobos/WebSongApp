70ad9169d5c6372af143e184bfa5b456667192eb
Alexi Lobos <a2020117797@estudiantes.upsa.edu.bo>
2021-10-12 23:54:22 -0400

    Migracion de modelo a la base de datos, Realizado

---
+++ WebSongApp/Migrations/20211013035116_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebSongApp.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cancion",
+                columns: table => new
+                {
+                    Cancion = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Letra = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Enlace = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cancion", x => x.Cancion);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cancion");
+        }
+    }
+}
